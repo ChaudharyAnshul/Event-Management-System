93f3a0644e64a54fa7dcafa94bcd24952ea43c00
AnshulChaudhary218 <53887551+AnshulChaudhary218@users.noreply.github.com>
2021-04-02 23:51:18 +0530

    login page initialized

--- EventManager/settings.py
+++ EventManager/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'EventManager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
