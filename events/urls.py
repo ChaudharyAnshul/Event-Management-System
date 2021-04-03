ede8f0a76a350f922899dce49f49449d4df3f362
AnshulChaudhary218 <53887551+AnshulChaudhary218@users.noreply.github.com>
2021-04-03 12:39:53 +0530

    merge conflict resolved

--- events/urls.py
+++ events/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.login, name='login'),
     path('accounts/', include('allauth.urls')),
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('profile/', views.profile, name='profile')
+    path('profile/', views.profile, name='profile'),
     path('registration/', views.registration, name='registration'),
     path('registration/staff', views.registrationStaff, name='registration staff'),
     path('registration/student', views.registrationStudent, name='registration student'),
